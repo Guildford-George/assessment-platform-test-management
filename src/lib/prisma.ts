be4724e23fa832fb8233ebe726cce70082e5c054
Guildford-George <georgeguildford1997@gmail.com>
2026-06-01 20:04:15 -1200

    feat: added database scheme

---
+++ src/lib/prisma.ts
@@ -0,0 +1,7 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "./generated/prisma/client";
+const connectString= process.env["DATABASE_URL"]!
+const adapter= new PrismaPg(connectString)
+const prisma= new PrismaClient({adapter})
+
+export default prisma
